0b9fb3cd93b86eade2ad1c2ac1e5cbf88fa45678
박시영 <pxy7571@gmail.com>
2023-12-31 13:34:28 +0900

    BAEKJOON 12/31

---
+++ src/Ex1065.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+//231231
+class Hansu{
+    public boolean findHansu(int i){
+        int h = i/100;
+        int t = (i-h*100)/10;
+        int o = i-h*100-t*10;
+        return h - t == t - o || o - t == t - h;
+    }
+}
+public class Ex1065 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Hansu ha = new Hansu();
+        int n = s.nextInt();
+        int i = 1;
+        int j = 0;
+        while(i<=n){
+            if (i<100) j++;
+            else {
+                if(ha.findHansu(i)) j++;
+            }
+            i++;
+        }
+        System.out.println(j);
+    }
+}
