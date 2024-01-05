5385e34137500e5cc0e751ada801cb656bfed3b3
박시영 <pxy7571@gmail.com>
2024-01-06 03:05:18 +0900

    BAEKJOON 24/01/05

---
+++ src/Ex2579.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//2024-01-05
+public class Ex2579 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [] stair = new int[N+1];
+        int [][] dp = new int[N+1][2];
+        for(int i=1; i<=N; i++){
+            stair[i] = Integer.parseInt(br.readLine());
+        }
+        if(N==1) {
+            System.out.println(stair[1]);
+            return;
+        }
+        dp[N][0] = stair[N];
+        dp[N][1] = stair[N];
+        int count = 1;
+        System.out.println(maxStair(stair, dp, count, N));
+    }
+    static int maxStair(int [] stair, int [][] dp, int count, int N){
+        if(N==dp.length) {
+            return Math.max(Math.max(dp[1][0], dp[1][1]), Math.max(dp[2][0],dp[2][1]));
+        }
+        if(N<2) return maxStair(stair, dp, count, dp.length);
+        if(dp[N-1][0] < dp[N][1] + stair[N-1] && count==1) {
+            dp[N-1][0]=dp[N][1] + stair[N-1];
+            count = 0;
+            maxStair(stair, dp, count, N-1);
+        }
+        count = 1;
+        if(dp[N-2][1] < dp[N][1] + stair[N-2]) {
+            dp[N-2][1] = dp[N][1] + stair[N-2];
+        }
+        if(dp[N-2][1] < dp[N][0] + stair[N-2]) {
+            dp[N-2][1] = dp[N][0] + stair[N-2];
+        }
+        if(dp[N-2][0] < dp[N][1] + stair[N-2]) {
+            dp[N-2][0] = dp[N][1] + stair[N-2];
+        }
+        if(dp[N-2][0] < dp[N][0] + stair[N-2]) {
+            dp[N-2][0] = dp[N][0] + stair[N-2];
+        }
+        maxStair(stair, dp, count, N-2);
+        return maxStair(stair, dp, count, dp.length);
+    }
+}
