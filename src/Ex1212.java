f043c383f4f4449fecaf5bf8eab88e7d03d71347
player7571 <pxy7571@gmail.com>
2024-03-20 00:26:38 +0900

    BAEKJOON 24/03/19

--- src/Ex1212.java
+++ src/Ex1212.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+//2024-03-19
 public class Ex1212 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
