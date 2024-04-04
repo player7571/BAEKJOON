fd7b57f64ab68bf7e7b6eec2dcb2671c0de383bc
player7571 <pxy7571@gmail.com>
2024-04-04 20:55:57 +0900

    BAEKJOON 24/04/04

---
+++ src/Ex9663.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//2024-04-04
+public class Ex9663 {
+    static int N;
+    static int cnt;
+    static int[] queen;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        cnt = 0;
+        if(N==1) {
+            System.out.print("1");
+            return;
+        }
+        if(N<=3) {
+            System.out.print("0");
+            return;
+        }
+        queen = new int[N+1];
+        findNqueen(1);
+        System.out.print(cnt);
+    }
+    //메모리 초과...
+    //백트래킹 써야됨
+    static void findNqueen(int level) {
+        if (level > N) {
+            cnt++;
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (isSafe(level, i)) {
+                queen[level] = i;
+                findNqueen(level + 1);
+            }
+        }
+    }
+
+    static boolean isSafe(int row, int col) {
+        for (int i = 1; i < row; i++) {
+            if (queen[i] == col || Math.abs(i - row) == Math.abs(queen[i] - col)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
